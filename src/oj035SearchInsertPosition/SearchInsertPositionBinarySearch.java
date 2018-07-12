6a26430ba88a2603075a129a16725baadf2aa97a
Xueying Bai <pureangelbxy@163.com>
2018-07-12 18:18:19 +0800

    Update SearchInsertPositionBinarySearch.java
    less than the smallest && greater than the greatest

--- src/oj035SearchInsertPosition/SearchInsertPositionBinarySearch.java
+++ src/oj035SearchInsertPosition/SearchInsertPositionBinarySearch.java
@@ -2,6 +2,12 @@ package oj035SearchInsertPosition;
 
 public class SearchInsertPositionBinarySearch {
 	public int searchInsert(int[] nums, int target) {
+	    if(nums.length == 0 || nums[0] > target) {
+                return 0;
+            }
+            if(nums[nums.length - 1] < target) {
+                return nums.length;
+            }
 		int low = 0;
 		int high = nums.length - 1;
 		while(low < high) {
